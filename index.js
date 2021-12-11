cf5b3f32a3690d77beb0106e8ea29664e8cefa8e
Kevin Paxton <kevinpaxton82@gmail.com>
2021-12-11 21:04:01 +0000

    Restructure of files

--- index.js
+++ index.js
@@ -28,49 +28,51 @@ const getDate = async () => {
 const loadFiles = async (year, day) => {
   try {
     challenge = require(`./${year}/${day}`);
-    [td1, rd1] = getData(`./${year}/${day}_1.txt`);
-    [td2, rd2] = getData(`./${year}/${day}_2.txt`);
-    run(challenge, td1, rd1, td2);
+    [testData, realData] = getData(`./${year}/${day}.txt`);
+    run(challenge, testData, realData);
   } catch {
     console.log(chalk.red(`Could not find AoC for ${day}/${year}`));
     await getDate();
   }
 };
 
-const run = async () => {
+const hasFunction = fn =>
+  typeof fn === 'function' && !fn.toString().endsWith('{}');
+
+const run = async (challenge, testData, realData) => {
   console.clear();
 
-  if (td1 || rd1) {
-    console.log(chalk.magenta('PART ONE'));
-    console.log('');
+  const passes = [
+    { heading: 'PART ONE', fn: 'partOne' },
+    { heading: 'PART TWO', fn: 'partTwo' },
+  ];
 
-    if (td1?.length) {
-      console.log(chalk.blue('With test data:'));
-      challenge.partOne(td1, { isTest: true, isReal: false, helpers, chalk });
+  for (const { heading, fn } of passes) {
+    if (hasFunction(challenge[fn])) {
+      console.log(chalk.magenta(heading));
       console.log('');
-    }
 
-    if (rd1?.length) {
-      console.log(chalk.green('With real data:'));
-      challenge.partOne(rd1, { isTest: false, isReal: true, helpers, chalk });
-      console.log('');
-    }
-  }
+      if (testData?.length) {
+        console.log(chalk.blue('With test data:'));
+        challenge[fn](testData, {
+          isTest: true,
+          isReal: false,
+          helpers,
+          chalk,
+        });
+        console.log('');
+      }
 
-  if (td2?.length || rd2?.length) {
-    console.log(chalk.magenta('PART TWO'));
-    console.log('');
-
-    if (td2?.length) {
-      console.log(chalk.blue('With test data:'));
-      challenge.partTwo(td2, { isTest: true, isReal: false, helpers, chalk });
-      console.log('');
-    }
-
-    if (rd2?.length) {
-      console.log(chalk.green('With real data:'));
-      challenge.partTwo(rd2, { isTest: false, isReal: true, helpers, chalk });
-      console.log('');
+      if (realData?.length) {
+        console.log(chalk.blue('With real data:'));
+        challenge[fn](realData, {
+          isTest: true,
+          isReal: false,
+          helpers,
+          chalk,
+        });
+        console.log('');
+      }
     }
   }
 };
